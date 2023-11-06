1de43b46e66c7f62163e68b3239f306fd91be8d7
ay571 <ay571@SonAhYoung>
2023-11-06 11:01:13 +0900

    init

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -13,14 +13,22 @@ import Signin from "./components/users/AccountSignIn";
 
 // Hotel
 import HotelList from "./components/users/HotelList";
-import Detail from "./components/users/Detail";
+import HotelDetail from "./components/users/HotelDetail";
+import HotelReserve from "./components/users/HotelReserve";
 import HotelInMap from "./components/users/HotelInMap";
+
+// Hotel Reserve Check
+import HotelReserveList from "./components/users/HotelReserveList";
+import HotelReserveDetail from "./components/users/HotelReserveDetail";
 
 // Board
 import BoardList from "./components/users/BoardList";
 import BoardDetail from "./components/users/BoardDetail";
 import BoardWrite from "./components/users/BoardWrite";
 import BoardUpdate from "./components/users/BoardUpdate";
+
+// Review
+import Review from './components/users/Review'
 
 // Manager
 import DashBoard from "./components/managers/DashBoard";
@@ -29,6 +37,7 @@ import Manager from "./components/managers/Manager";
 // Admin
 import DashBoardAdmin from "./components/admins/DashBoard";
 import Admin from "./components/admins/Admin";
+import Info from "./components/users/Info";
 
 
 function App() {
@@ -38,14 +47,19 @@ function App() {
         <Route path="/">
           <Route index element={<Main />} /> 
           <Route path="hotelList" element={<HotelList />} />
-          <Route path="detail" element={<Detail />} />
+          <Route path="detail" element={<HotelDetail />} />
           <Route path="hotelInMap" element={<HotelInMap />} />
+          <Route path="hotelReserve" element={<HotelReserve />} />
+          <Route path="hotelReserveList" element={<HotelReserveList />} />
+          <Route path="hotelReserveListDetail" element={<HotelReserveDetail />} />
           <Route path="login" element={<Login />} />
           <Route path="signin" element={<Signin />} />
           <Route path="boardList" element={<BoardList />} />
           <Route path="boardDetail" element={<BoardDetail />} />
           <Route path="boardWrite" element={<BoardWrite />} />
           <Route path="boardUpdate" element={<BoardUpdate />} />
+          <Route path="review" element={<Review />} />
+          <Route path="info" element={<Info />} />
         </Route>
         <Route path="/manager">
           <Route index element={<DashBoard />} />
