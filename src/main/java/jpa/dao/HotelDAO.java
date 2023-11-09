85b0a0a27e93d4e2ab27de98b55c11ee9cef5715
YOUNG-NIM <ay57105710@gmail.com>
2023-11-09 15:22:13 +0900

    [FIX] dto

--- src/main/java/jpa/dao/HotelDAO.java
+++ src/main/java/jpa/dao/HotelDAO.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import jpa.bean.HotelCategoryDTO;
 import jpa.bean.HotelDTO;
 
 public interface HotelDAO extends JpaRepository<HotelDTO, Integer> {
