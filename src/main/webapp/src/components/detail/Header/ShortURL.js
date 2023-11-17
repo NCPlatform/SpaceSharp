35f631620a0cb3b2df7f2b89aef60b712baaeb8f
euncham <first00125@naver.com>
2023-11-17 10:40:52 +0900

    [FIX]index.html

--- src/main/webapp/src/components/detail/Header/ShortURL.js
+++ src/main/webapp/src/components/detail/Header/ShortURL.js
@@ -11,7 +11,7 @@ const ShortURL = () => {
         let key = 'JxJ9Y4wLIUJuQTj1YwxwrL5Z5YqX7BA2ThQ3k8ob' // NCP 콘솔에서 서비스 신청 후 발급
         axios.post('https://naveropenapi.apigw.ntruss.com/util/v1/shorturl', null, {
             params: {
-                url: "http://223.130.136.134:8090/NetFlex/",
+                url: "https://www.naver.com",
                 //shortURL의 경우 localhost:3000에서는 동작하지 않습니다. 배포 후 window.location.href로 바꿔 주세요
                 // 저 URL은 저희 미니프로젝트 링크입니다! 컴포넌트 추가 후 정상 접속되는지 테스트 부탁드립니다
                 "X-NCP-APIGW-API-KEY-ID": id,
