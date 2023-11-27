f640eede8b1a8df2b8e6d60e0369ba50e463a239
YOUNG-NIM <ay57105710@gmail.com>
2023-11-27 10:17:50 +0900

    [FIX] Coupon

--- src/main/java/jpa/dao/CouponDAO.java
+++
@@ -1,9 +0,0 @@
-package jpa.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import jpa.bean.CouponDTO;
-
-public interface CouponDAO extends JpaRepository<CouponDTO, Integer> {
-
-}
